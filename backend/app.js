8a79bc76cbd2725eeae46d3910725f19ceffd8e6
ramankc6 <ramankc699@gmail.com>
2024-03-24 07:30:24 -0400

    api working

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,12 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const OpenAIApi = require('openai')
+const { verifyEmailAndPassword, initializeFirebase, addSubscription } = require('./firebase-service')
+const openai = new OpenAIApi.OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+initializeFirebase()
 app.post('/incoming', (req, res) => {
   console.log("inc running")
   res.status(200);
@@ -28,8 +34,7 @@ app.post('/incoming', (req, res) => {
   `);
 });
 
-app.get('/api/summurizeTOS', async (req, res) => {
-  const { tos } = req.body
+const summarizeTOS = async (tos) => {
   const messages = [{ role: "system", content: "You are CancelGPT. CancelGPT is designed to analyze Terms of Service (TOS) documents for companies and provide concise summaries of the terms related to cancellation. It extracts all details pertinent to cancellation, such as conditions, deadlines, fees, and procedures, presenting them in a summary that optimizes for minimal token usage. This approach ensures the essential information is conveyed efficiently, ready for further processing by other GPT instances. CancelGPT prioritizes clarity and precision, focusing solely on cancellation terms without including unnecessary details. It will ask for clarifications only when the provided TOS lacks clear cancellation terms or when essential details are ambiguous, ensuring the summaries are both compact and comprehensive. The tone is straightforward and factual, aimed at distilling relevant information without embellishment. Word it in a way that the summary can be read before contacting customer service to cancel their subscription." }, { role: "user", content: tos }]
   try {
     const response = await openai.chat.completions.create({
@@ -37,14 +42,12 @@ app.get('/api/summurizeTOS', async (req, res) => {
       messages: messages
     })
     const aiResponse = response.choices[0].message.content
-    console.log(aiResponse)
-
-    res.json(aiResponse)
+    return aiResponse
   } catch (error) {
     console.error('Failed to send message', error)
     res.status(500).send('Server error')
   }
-})
+}
 
 
 app.post('/api/getTOS', async (req, res) => {
