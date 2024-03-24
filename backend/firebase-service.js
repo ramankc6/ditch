49391489d43e1361371b0b8bd664963bde606f01
Xi Chen <963929975@qq.com>
2024-03-24 02:43:17 -0400

    modified firebase api

--- backend/firebase-service.js
+++ backend/firebase-service.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin')
+// const firebase = require("firebase/app")
 
 let serviceAccount = require('./hoohack-c24ae-firebase-adminsdk-jrhu8-e81b4ca042.json')
 
@@ -6,6 +7,7 @@ const initializeFirebase = () => {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   })
+  console.log("Firebase initialized")
 }
 
 const addSubscription = async (email, subscription) => {
@@ -14,13 +16,20 @@ const addSubscription = async (email, subscription) => {
   await docRef.set({ subscriptions: admin.firestore.FieldValue.arrayUnion(subscription) })
 }
 
+const addTOS = async (email, TOS) => {
+  const id = await admin.auth().getUserByEmail(email)
+  const docRef = db.collection('users').doc(id)
+  await docRef.set({ TOS: admin.firestore.FieldValue.arrayUnion(TOS) })
+}
+
 async function verifyEmailAndPassword (email, password) {
-  try {
-    const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
-    console.log("Authentication successful", userCredential.user)
-  } catch (error) {
-    console.error("Authentication failed", error)
-  }
+  // try {
+  //   const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
+  //   console.log("Authentication successful", userCredential.user)
+  //   return userCredential
+  // } catch (error) {
+  //   console.error("Authentication failed", error)
+  // }
 }
 
 module.exports = {
